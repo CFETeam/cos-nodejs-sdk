114976b4c676acc2aac9c53a00c16aae35a187d0
miusuncle <miusuncle@gmail.com>
2016-11-15 17:35:36 +0800

    bugfix & formatting

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-
 var libpath = './lib';
 
 module.exports = {
-  auth:     require(libpath + '/auth.js'),
-  conf:     require(libpath + '/conf.js'),
-  cos:      require(libpath + '/cos.js'),
+  auth: require(libpath + '/auth.js'),
+  conf: require(libpath + '/conf.js'),
+  cos: require(libpath + '/cos.js'),
 };
